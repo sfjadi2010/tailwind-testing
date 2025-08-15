6465c93f53a131f8033dfba6328181af7804a724
Salman Farsi <sfjadi2010@live.com>
2025-08-15 19:18:44 -0400

    Enhance Hero component with subscription input and promotional message

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,6 +15,22 @@ const Hero: React.FC = () => {
           <div className="relative">
             <img src={logo} alt="logo" className="w-[175px]" />
           </div>
+          <div className="text-5xl font-bold">
+            tw:<span className="text-sky-900">mf</span>
+          </div>
+        </div>
+        <div className="text-sm mt-3 font-bold">
+          Keep me updated with news and promotions
+        </div>
+        <div className="mt-3 flex items-center justify-center">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="p-2 rounded-l-sm border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500"
+          />
+          <button className="bg-pink-500 text-white px-2 py-2 rounded-r-sm hover:bg-pink-600 transition-colors">
+            Subscribe
+          </button>
         </div>
       </div>
     </>
